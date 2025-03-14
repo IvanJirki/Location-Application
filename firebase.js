3f75e6aa5513f745292c3f82744c709fdd491c45
IvanJirki <c3jiiv00@students.oamk.fi>
2025-03-14 22:37:50 +0200

    pahasti kesken mutten saa sitä enempää tehtyä, jotenki koodit ei toimi ja tietoa en löydä

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: 'your-api-key',
-  authDomain: 'your-auth-domain',
-  projectId: 'your-project-id',
-  storageBucket: 'your-storage-bucket',
-  messagingSenderId: 'your-sender-id',
-  appId: 'your-app-id',
+  apiKey: 'POST https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=API_KEY',
+  authDomain: '',
+  projectId: 'location-application-46dfa',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
-// Initialize Firebase
+// Alusta Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
